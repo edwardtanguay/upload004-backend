f8d91baa265dc623b209b97fb70af585861515f4
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-31 11:27:31 +0100

    no errors in server.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,1 @@
 export const port = 3610;
-export const logPathAndFileName = './src/logs/log.txt';
--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,67 @@
-import * as model from './model.js';
 import express from 'express';
 import cors from 'cors';
-import logger from './logger.js';
 import * as config from './config.js';
+import multer from 'multer'; // uploading
+import { join, dirname } from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+// TODO: consider upgrade from 3.0 to 5.0 lowdb
+import { Low, JSONFile } from 'lowdb'; // database
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const dbFile = join(__dirname, '/data/db.json');
+const adapter = new JSONFile(dbFile);
+const db = new Low(adapter);
 
 const app = express();
-app.use(cors());
-app.use(logger);
+const port = 5889;
 
-app.get('/', (req: express.Request, res: express.Response) => {
-	res.send(model.getApiInstructions());
-});
+const staticDirectory = path.join(__dirname, './public');
+app.use(express.static(staticDirectory));
 
-app.get('/books', (req: express.Request, res: express.Response) => {
-	res.json(model.getBooks());
-});
-
-app.get('/books/:id', (req: express.Request, res: express.Response) => {
-	const id = Number(req.params.id);
-	if (isNaN(id)) {
-		res.status(400).send({
-			error: true,
-			message: "sent string, should be number"
-		});
-	} else {
-		const book = model.getBook(id);
-		if (book === undefined) {
-			res.status(404).send({
-				error: true,
-				message: "id did not correspond to an existing item"
-			});
-		} else {
-			res.json(book);
-		}
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'public/uploadedFiles/');
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
 	}
+});
+
+const upload = multer({ storage });
+
+app.use(cors());
+
+// TODO: resolve all TypeScript any types
+// TODO: put icons in their own directory, not uploadedFiles
+app.get('/fileitems', async (req, res) => {
+	await db.read();
+	res.send((db.data as any).fileItems);
+});
+
+app.post('/uploadfile', upload.single('file'), async (req, res) => {
+	await db.read();
+	const fileName = req.body.fileName;
+	let iconPathAndFileName = '';
+	if (fileName.endsWith('.xlsx')) {
+		iconPathAndFileName = 'images/general/iconExcel.png';
+	} else if (fileName.endsWith('.json')) {
+		iconPathAndFileName = 'images/general/iconJson.png';
+	} else if (fileName.endsWith('.txt')) {
+		iconPathAndFileName = 'images/general/iconText.png';
+	} else {
+		iconPathAndFileName = `uploadedFiles/${fileName}`
+	}
+
+	(db.data as any).fileItems.push({
+		title: req.body.title,
+		description: req.body.description,
+		notes: req.body.notes,
+		fileName: req.body.fileName,
+		iconPathAndFileName
+	});
+	await db.write();
+	res.json({});
 });
 
 app.listen(config.port, () => {
